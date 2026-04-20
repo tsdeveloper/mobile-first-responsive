a1e839b0ce1106bc03583eeac539744b34f09c60
geek <ts.analista@gmail.com>
2026-04-20 17:40:19 -0300

    feat: add tag "a"

--- src/shared/layout/header/ui/Header.tsx
+++ src/shared/layout/header/ui/Header.tsx
@@ -1,6 +1,3 @@
-import {MagnifyingGlassIcon, ShoppingBagIcon, UserIcon} from "@heroicons/react/24/outline";
-import ReactCountryFlag from "react-country-flag";
-
 export const Header = () => (
     <header className="site-header">
             <div className="site-header__brand">
@@ -10,13 +7,31 @@ export const Header = () => (
             <div className="site-header__nav">
                 <div className="site-header__nav-list">
                     <div className="site-header__nav-item">
-                        <div className="site-header__nav-link">Home</div>
-                        <div className="site-header__nav-link">About</div>
-                        <div className="site-header__nav-link">Services</div>
-                        <div className="site-header__nav-link">Works</div>
-                        <div className="site-header__nav-link">Gallery</div>
-                        <div className="site-header__nav-link">Blog</div>
-                        <div className="site-header__nav-link">Contact</div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Home</a></div>
+                        <div className="site-header__nav-link">
+                            <a href="#">About
+                            </a>
+                            </div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Services
+                            </a>
+                            </div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Works
+                            </a>
+                            </div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Gallery
+                            </a>
+                            </div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Blog
+                            </a>
+                            </div>
+                        <div className="site-header__nav-link">
+                            <a href="#">Contact
+                            </a></div>
                     </div>
                 </div>
 
